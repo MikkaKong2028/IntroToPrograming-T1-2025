670b9816bff7ec6b4a82a474399051782d7609ed
Kong Mikka <833145@stma.k12.mn.us>
2025-09-25 13:15:26 -0500

    iajhds

---
+++ practice/2.3-practice.py
@@ -0,0 +1,42 @@
+print(2 + 2)
+print(2*2)
+# string     - "in quotatons"
+# integer    - a whole number ex. 1 4 -10
+# float      - a decimal number ex. 000000.1
+# char       - a single glyph ex. 'c'
+# bool       - true or false
+# parameter  - info the code of line to run
+
+
+# how to define a variable 
+# <name> + <value>
+
+x = 10
+print(x)
+
+fontSize = 14
+
+print(fontSize)
+print(fontSize)
+print(fontSize)
+
+x = 10
+y = 50
+potato=7
+print(x * y * potato)
+
+# naming convention  - recomodation or a norm 
+
+lowercase = True
+UPPERCASE = True
+camelCase = True    # All lowercase, no spaces, capital for new words
+snake_case = True   # All lowercase, underscores for spaces. 
+SCREAMING_SNAKE_CASE = False 
+
+# Other genral rules to naming things
+# 1. Consise 
+#   -Short and discripted 
+
+
+
+
