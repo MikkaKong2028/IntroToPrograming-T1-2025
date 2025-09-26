85d98f48a120f6c317b026649b9685671d740d2f
Kong Mikka <833145@stma.k12.mn.us>
2025-09-26 14:01:34 -0500

    Animal Survey

---
+++ assignments/animalsurvey.py
@@ -0,0 +1,17 @@
+animal = input("What is your favorite animal?\n>")
+like = input("Why do you like this animal\n>")
+where = input("Where does it live\n>")
+fun_fact = input("Whats a fun fact about your animal\n>")
+
+
+
+
+
+print("your favorite animal is a " +animal+ " you like this animal because " +like+ " It lives in " +where+ " a fun fact " +fun_fact+ "."
+"")
+
+
+
+
+
+
