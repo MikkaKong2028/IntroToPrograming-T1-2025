bda50da75afb2586612175703d0d378448754cfd
Kong Mikka <833145@stma.k12.mn.us>
2025-09-29 13:14:16 -0500

    comments

--- assignments/animalsurvey.py
+++ assignments/animalsurvey.py
@@ -10,7 +10,13 @@ fun_fact = input("Whats a fun fact about your animal\n>")
 print("your favorite animal is a " +animal+ " you like this animal because " +like+ " It lives in " +where+ " a fun fact " +fun_fact+ ".")
 
 
+"""
+Ive made the anwser into a sentance so that when you get your survey
+back it reads it to you in a sentence and not just a list. 
 
+
+
+"""
 
 
 
