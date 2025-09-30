002e38f676dc9f8b0ae93abc3069e924c516302e
Kong Mikka <833145@stma.k12.mn.us>
2025-09-30 13:34:13 -0500

    Madlib2

--- assignments/madlib.py
+++ assignments/madlib.py
@@ -2,7 +2,7 @@
 """
 Once upon a time there was a brave [noun] that ventured into a [adjective] , [noun]. 
 One day as it was venturing into this place it stumbled upon treasure. 
-this treasure was a [noun] and it brpoung great [emotion] to its life. 
+this treasure was a [noun] and it brought great [emotion] to its life. 
 After finding such treasure it [ending ly word] [verb].
 
 
@@ -12,7 +12,7 @@ noun_1 = input("noun 1 (thing)\n>")
 adjective = input("adjective\n>")
 noun_2 = input("noun 2 (place)\n>")
 noun_3 = input("noun 3 (thing)\n>")
-emotion = input("emotion\n>")
+emotion = input("emotion what is it feeling, ending in (ness) (er))\n>")
 LY = input("word ending in ly\n>")
 verb = input("verb (action)\n>")
             
