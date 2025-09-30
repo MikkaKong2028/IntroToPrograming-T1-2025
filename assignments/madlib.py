d949b0d96c8d273da8fc39c745366658618be5f8
Kong Mikka <833145@stma.k12.mn.us>
2025-09-30 13:35:21 -0500

    madlibfinal

--- assignments/madlib.py
+++ assignments/madlib.py
@@ -12,7 +12,7 @@ noun_1 = input("noun 1 (thing)\n>")
 adjective = input("adjective\n>")
 noun_2 = input("noun 2 (place)\n>")
 noun_3 = input("noun 3 (thing)\n>")
-emotion = input("emotion what is it feeling, ending in (ness) (er))\n>")
+emotion = input("emotion what is it feeling, ending in (ness) (er)\n>")
 LY = input("word ending in ly\n>")
 verb = input("verb (action)\n>")
             
