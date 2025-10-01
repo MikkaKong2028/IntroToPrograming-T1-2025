ad0368c3447691a720ca003c453ddfbcbcdf1c51
Kong Mikka <833145@stma.k12.mn.us>
2025-10-01 13:59:16 -0500

    jaksjd

--- practice/caculate-tax.py
+++ practice/caculate-tax.py
@@ -1,6 +1,6 @@
 
 def calculate_tax(item, price, rate):
-    print()"the amount of tax to be collected on a " + str(price) + " dollar " + item + " is " + str((price*rate))
+    print("the amount of tax to be collected on a " + str(price) + " dollar " + item + " is " + str((price*rate))
 
 
 calculate_tax("starbucks", 7.99, 0.6825)
