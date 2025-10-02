92ba3de5b376a263b77aa153aae55ee93b962394
Kong Mikka <833145@stma.k12.mn.us>
2025-10-02 14:10:36 -0500

    madlib

--- assignments/madlib.py
+++ assignments/madlib.py
@@ -17,7 +17,6 @@ ly = input("word ending in ly\n>")
 verb = input("verb (action)\n>")
             
 
-print("Once upon a time there was a brave " +noun_1+ " that ventured into a " +adjective+ ", " +noun_2+ ". One day as it was venturing into this place it stumbled upon treasure. this treasure was a " +noun_3+ " and it brought great " +emotion+ " to its life. After finding such treasure it " +LY+ " "+verb+ ".")
 
 
 
@@ -25,7 +24,7 @@ def mad_lib(noun_1, adjective, noun_2, noun_3, emotion, ly, verb):
     print("Once upon a time there was a brave " +noun_1+ " that ventured into a " +adjective+ ", " +noun_2+ ". One day as it was venturing into this place it stumbled upon treasure. this treasure was a " +noun_3+ " and it brought great " +emotion+ " to its life. After finding such treasure it " +ly+ " "+verb+ ".")
 
 
-mad_lib( )
+mad_lib(noun_1, adjective, noun_2, noun_3, emotion, ly, verb )
 
 
 
